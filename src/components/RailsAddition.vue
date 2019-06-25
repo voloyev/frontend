e0b8a79bb29bdefdd8ff8cede247442870e71099
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2019-06-25 03:43:57 +0300

    Add rails add comp realization

---
+++ src/components/RailsAddition.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>
+    <a :href="getLink(link)"> {{ title }} <span>from Rails</span> </a>
+  </p>
+</template>
+
+<script>
+const baseUrl = "http://localhost:3000/api/v1/product/";
+export default {
+  props: ["title", "link"],
+  methods: {
+    getLink(link) {
+      return baseUrl + link;
+    }
+  }
+};
+</script>
+<style scoped>
+span {
+  color: red;
+  font-weight: bold;
+}
+</style>
